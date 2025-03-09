ae3479a3a5ebf6c82bea98808bfc64b97a6cf242
Demon <505159718@qq.com>
2025-03-09 20:47:58 +0800

    fix: 修复服务端渲染问题

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,16 @@
 'use client'
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useRouter } from 'next/navigation'
 
 export default function AuthPage({ children }: { children: React.ReactNode }) {
-    const [token] = useState(localStorage.getItem('token'))
     const router = useRouter()
     useEffect(() => {
+        const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
         if (!token) {
             router.replace('/login') // 如果没有 Token，跳转到登录页
         }
-    }, [token])
+    }, [])
     return <>
-        {token && children}
+        {children}
     </>
 }
